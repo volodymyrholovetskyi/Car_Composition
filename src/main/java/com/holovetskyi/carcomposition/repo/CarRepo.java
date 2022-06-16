25e5d8b88ec15037929d4b8b00abd90a7b556aee
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-06-16 22:14:41 +0200

    add file parameter.properties

--- src/main/java/com/holovetskyi/carcomposition/repo/CarRepo.java
+++ src/main/java/com/holovetskyi/carcomposition/repo/CarRepo.java
@@ -1,8 +1,13 @@
 package com.holovetskyi.carcomposition.repo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CarRepo {
+
+    @Value("${repo.filename}")
+    String filename;
+
 
 }
