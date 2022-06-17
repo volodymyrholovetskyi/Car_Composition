a2a2b06a8ef4fddc45f2cef825018776a563594f
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-06-17 18:54:54 +0200

    add layer validator

---
+++ src/main/java/com/holovetskyi/carcomposition/validate/Validator.java
@@ -0,0 +1,24 @@
+package com.holovetskyi.carcomposition.validate;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public interface Validator<T> {
+
+    Log LOG = LogFactory.getLog(Validator.class.getName());
+
+    Map<String, String> validate(T t);
+
+    static <T> boolean validate(Validator<T> validator, T t) {
+        var errors = validator.validate(t);
+        LOG.error(errors
+                .entrySet()
+                .stream()
+                .map(e -> e.getKey() + ": " + e.getValue())
+                .collect(Collectors.joining("\n")));
+        return errors.isEmpty();
+    }
+}
