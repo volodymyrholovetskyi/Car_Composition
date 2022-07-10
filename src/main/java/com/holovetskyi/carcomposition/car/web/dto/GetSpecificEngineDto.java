2ed987282fd07cde8e1738f6a56dfd28857eacf8
Volodymyr <volodymyrgolovetskyi1995@gmail.com>
2022-07-10 18:28:07 +0200

    add enum EngineTypeDto

--- src/main/java/com/holovetskyi/carcomposition/car/web/dto/GetSpecificEngineDto.java
+++ src/main/java/com/holovetskyi/carcomposition/car/web/dto/GetSpecificEngineDto.java
@@ -1,14 +1,16 @@
 package com.holovetskyi.carcomposition.car.web.dto;
 
+import com.holovetskyi.carcomposition.car.web.dto.type.EngineTypeDto;
 import lombok.Data;
 
 @Data
     public class GetSpecificEngineDto {
 
         String engine;
+        EngineTypeDto typeDto;
 
         public GetCriterionDto (String engine) {
-
+            this.typeDto = checkCriterion(engine);
         }
 
 }
---
+++ src/main/java/com/holovetskyi/carcomposition/car/web/dto/type/EngineTypeDto.java
@@ -0,0 +1,4 @@
+package com.holovetskyi.carcomposition.car.web.dto.type;
+
+public enum EngineTypeDto {
+}
